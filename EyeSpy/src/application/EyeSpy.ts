0b64bd64b5f4d4e26daf4cfbdd1596f4d0eb3503
Martin Stephen Cox <admin@martinstephencox.com>
2017-04-02 12:53:26 +0100

    Reorganised structure. Add tab change logging.

---
+++ EyeSpy/src/application/EyeSpy.ts
@@ -0,0 +1,10 @@
+﻿/**
+ * Responds to "tabLoaded" events triggered by the Background script.
+ */
+chrome.runtime.onMessage.addListener((message: any, sender: chrome.runtime.MessageSender, sendResponse: any) =>
+{
+	if (message.action === "tabLoaded")
+	{
+		console.log("A Tab has loaded: " + message.url);
+	}
+});
