c1bb7f09d9c12898737f81eac35ce27b3c7f44fe
Martin Stephen Cox <admin@martinstephencox.com>
2017-04-05 22:40:25 +0100

    Added simple Clarifai support that logs concepts in the first image.

--- EyeSpy/src/application/PageProcessor.ts
+++ EyeSpy/src/application/PageProcessor.ts
@@ -1,12 +1,26 @@
 ﻿import * as $ from "jquery";
 
+import { CLIENT_ID, CLIENT_SECRET } from "./Keys";
+
+// TODO: Get Clarifai to be webpack compatible.
+const Clarifai = require("clarifai");
+
 /**
  * The PageProcessor class. Handles logic for processing page DOM elements.
  */
 export class PageProcessor
 {
-	/** The array of jQuery image elements. */
-	private _imageElement: JQuery[];
+	/** The jQuery image elements on the page. */
+	private _imageElements: JQuery;
+
+	// TODO: When Clarifai is webpack compatible, the clarifai instance should be moved to EyeSpyController.
+	/** The Clarifai app instance. */
+	private _clarifai: any;
+
+	public constructor()
+	{
+		this._clarifai = new Clarifai.App(CLIENT_ID, CLIENT_SECRET);
+	}
 
 	/**
 	 * Processes a page.
@@ -31,7 +45,29 @@ export class PageProcessor
 	 */
 	private _getPageImages(): void
 	{
-		console.log("Page scan results:");
-		console.log($("body").find("img"));
+		this._imageElements = $("body").find("img");
+
+		// Test clarifai on the first available image.
+		const firstImageSource = this._imageElements.first().attr("src");
+
+		if (firstImageSource)
+		{
+			this._clarifai.models.predict(Clarifai.GENERAL_MODEL, firstImageSource).then(
+				(response: any) =>
+				{
+					const imageConcepts: any[] = response.outputs[0].data.concepts;
+
+					imageConcepts.forEach((concept: any) =>
+					{
+						console.log("Name: " + concept.name);
+						console.log("Probability: " + concept.value * 100 + "%");
+					});
+				},
+				(error: any) =>
+				{
+					console.error(error);
+				}
+			);
+		}
 	}
 }
