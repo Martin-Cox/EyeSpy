28a7f7b2f1b325f86155ff6abf2319c2ba8ceed4
Martin Stephen Cox <admin@martinstephencox.com>
2017-04-08 13:01:44 +0100

    Display image concepts on the page when analysing image via context menu. This is a quick and dirty implementation for demonstration purposes.

--- EyeSpy/src/application/tab/PageProcessor.ts
+++ EyeSpy/src/application/tab/PageProcessor.ts
@@ -10,8 +10,11 @@ const clarifai = require("clarifai");
  */
 export class PageProcessor
 {
-	/** The jQuery image elements on the page. */
+	/** The JQuery image elements on the page. */
 	private _imageElements: JQuery;
+
+	/** The JQuery element that was target by a mouse event. */
+	private _mouseTargetElement: JQuery;
 
 	/** The Clarifai app instance. */
 	private _clarifai: any;
@@ -19,6 +22,15 @@ export class PageProcessor
 	public constructor()
 	{
 		this._clarifai = new clarifai.App(CLIENT_ID, CLIENT_SECRET);
+
+		// Listen to right click mousedown events on the page and update the targetElement.
+		$("body").mousedown((event: JQueryMouseEventObject) =>
+		{
+			if (event.button === 2)
+			{
+				this._mouseTargetElement = $(event.target);
+			}
+		});
 	}
 
 	/**
@@ -35,10 +47,13 @@ export class PageProcessor
 	 */
 	public analyseImage(image: string | JQuery): void
 	{
-		// Get the url of the image.
-		const url = (typeof (image) === "string") ? image : image.attr("src");
+		let url: string;
 
-		this._predictImage(url);
+		// Get the url of the image.
+		url = (typeof (image) === "string") ? image : image.attr("src");
+
+		// TODO: If we pass in image as a JQuery element we should pass that through here instead of the _mouseTargetElement
+		this._predictImage(url, this._mouseTargetElement);
 	}
 
 	/**
@@ -71,18 +86,27 @@ export class PageProcessor
 	 * Predicts the contents of a single image.
 	 * @param url The source of the image to predict.
 	 */
-	private _predictImage(url: string): void
+	private _predictImage(url: string, image?: JQuery): void
 	{
 		this._clarifai.models.predict(clarifai.GENERAL_MODEL, url).then(
 			(response: any) =>
 			{
-				const imageConcepts: any[] = response.outputs[0].data.concepts;
-
-				imageConcepts.forEach((concept: any) =>
+				if (image)
 				{
-					console.log("Name: " + concept.name);
-					console.log("Probability: " + concept.value * 100 + "%");
-				});
+					this._displayImageConcepts(image, response.outputs[0].data.concepts);
+				}
+				else
+				{
+					// There is no image element we can use to display the concepts on, just log them to the console instead.
+					const imageConcepts: any[] = response.outputs[0].data.concepts;
+
+					imageConcepts.forEach((concept: any) =>
+					{
+						console.log("Name: " + concept.name);
+						console.log("Probability: " + (concept.value * 100).toFixed(2) + "%");
+					});
+				}
+
 			},
 			(error: any) =>
 			{
@@ -90,4 +114,28 @@ export class PageProcessor
 			}
 		);
 	}
+
+	/**
+	 * Display image concepts on the page.
+	 * @param image The JQuery image element to display the concepts for.
+	 * @param concepts The concepts to display.
+	 */
+	private _displayImageConcepts(image: JQuery, concepts: [any]): void
+	{
+		const topConcepts     = concepts.slice(0, 5);
+		const conceptsWrapper = $("<div class=\"eyespy-image-concepts-wrapper\">Image Contents:</div>");
+
+		image.replaceWith(conceptsWrapper);
+
+		topConcepts.forEach((concept: any) =>
+		{
+			const conceptElement = $("<div class=\"eyespy-image-concept\"><p>" + concept.name
+				+ " " + (concept.value * 100).toFixed(2) + "%</p></div>");
+
+			conceptsWrapper.append(conceptElement);
+		});
+	}
 }
+
+// TODO: Instead of passing in an image element to _predictImage and letting that decide if it is suitable to
+// _displayImageConcepts, _predictImage should return a promise so that whatever called it can decide how to react.
